886fae391f00c1d7287c4de83ce5d4a60d3d3e4e
Greg Mat. <65409906+itsvyle@users.noreply.github.com>
2024-06-30 11:23:17 +0000

    Add basic filtering

--- fg.go
+++ fg.go
@@ -10,7 +10,7 @@ import (
 )
 
 var maxLength = flag.Int("max", 160, "max length of the fortune")
-var maxLines = flag.Int("lines", 15, "max lines of the fortune")
+var maxLines = flag.Int("lines", 5, "max lines of the fortune")
 var sourceDirectory = flag.String("dir", "/usr/share/games/fortunes", "source directory of the fortunes")
 var outputDirectory = flag.String("out", "", "output directory of the fortunes (defaults for dir/dat)")
 
@@ -41,10 +41,65 @@ func main() {
 		}
 	}
 
-	log.Println("Found", len(files), "files")
-	log.Println(files)
+	log.Println("Found", len(files), "files:", files)
+
+	// create output directory
+	err = os.MkdirAll(*outputDirectory, 0755)
+	if err != nil {
+		fmt.Println("Error creating output directory:", err)
+		return
+	}
+	for _, f := range files {
+		path := filepath.Join(*sourceDirectory, f)
+		// read contents
+		contents, err := os.ReadFile(path)
+		if err != nil {
+			fmt.Println("Error opening file:", err)
+			return
+		}
+		newFile, err := filterFile(f, string(contents))
+		if err != nil {
+			fmt.Println("Error filtering file:", err)
+			return
+		}
+
+		// write file
+		err = os.WriteFile(filepath.Join(*outputDirectory, f), []byte(newFile), 0644)
+		if err != nil {
+			fmt.Println("Error writing file:", err)
+			return
+		}
+	}
+
 }
 
 func isExecutable(fileInfo os.DirEntry) bool {
 	return (fileInfo.Type().Perm() & 0111) != 0
+}
+
+const fortuneSeparator = "\n%\n"
+
+func filterFile(fileName string, originalContent string) (string, error) {
+	newFile := ""
+	fortunes := strings.Split(originalContent, fortuneSeparator)
+	log.Println("["+fileName+"]", "Found", len(fortunes), "fortunes in file")
+
+	newCount := 0
+	for _, line := range fortunes {
+		if line == "" {
+			continue
+		}
+		if strings.Count(line, "\n") > *maxLines {
+			continue
+		}
+		if len(line) > *maxLength {
+			continue
+		}
+		newFile += line + fortuneSeparator
+		newCount++
+	}
+	newFile = strings.TrimSuffix(newFile, fortuneSeparator)
+	log.Println("["+fileName+"]", "Filtered to", newCount, "fortunes")
+
+	return newFile, nil
 }
